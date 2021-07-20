c1847d006688d1d4729f636425a2e620e1e31fe6
YZWong <wyzhi02@gmail.com>
2021-07-20 16:45:38 +0800

    Executable program, neeed more tests & fix text alignment

--- PSAP-Assignment/PSAP-Assignment/main.cpp
+++ PSAP-Assignment/PSAP-Assignment/main.cpp
@@ -1,8 +1,129 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
+int randomIntegerGenerator() {
+	int randomNumber = (rand() % 10) + 1;
+	return randomNumber;
+}
+
+int calculateRunnerOnePosition(int position_runner, int randomNumber) {
+	if (randomNumber >= 1 && randomNumber <= 5)
+	{
+		position_runner += 3;
+		cout << "Runner 1 uses SPRINT and move 3 squares ahead." << endl;
+	}
+	else if (randomNumber >= 6 && randomNumber <= 7)
+	{
+		position_runner += 1;
+		cout << "Runner 1 uses JOG and move 1 square ahead." << endl;
+	}
+	else
+	{
+		if (position_runner <= 1)
+		{
+			position_runner = 1;
+			cout << "Runner 1 uses WALK and return to starting point." << endl;
+		}
+		else
+		{
+			position_runner -= 6;
+			cout << "Runner 1 uses WALK and move 6 squares behind." << endl;
+		}
+	}
+
+	return position_runner;
+}
+
+int calculateRunnerTwoPosition(int position_runner, int randomNumber) {
+	if (randomNumber >= 1 && randomNumber <= 3)
+	{
+		position_runner += 5;
+		cout << "Runner 2 uses FAST SPRINT and move 5 squares ahead." << endl;
+	}
+	else if (randomNumber >= 4 && randomNumber <= 5)
+	{
+		position_runner += 3;
+		cout << "Runner 2 uses RUN and move 3 squares ahead." << endl;
+	}
+	else if (randomNumber == 6)
+	{
+		if (position_runner <= 1)
+		{
+			position_runner = 1;
+			cout << "Runner 1 uses WALK and return to starting point." << endl;
+		}
+		else
+		{
+			position_runner -= 2;
+			cout << "Runner 2 uses WALK and move 2 squares behind." << endl;
+		}
+	}
+	else if (randomNumber >= 7 && randomNumber <= 8)
+	{
+		if (position_runner <= 1)
+		{
+			position_runner = 1;
+			cout << "Runner 1 uses CRAWL and return to starting point." << endl;
+		}
+		else
+		{
+			position_runner -= 4;
+			cout << "Runner 2 uses CRAWL and move 4 squares behind." << endl;
+		}
+	}
+	else
+	{
+		cout << "Runner 2 uses SLEEP and nothing happens." << endl;
+	}
+
+	return position_runner;
+}
+
+void showRunnersLocation(int position_runnerOne, int position_runnerTwo) {
+	if (position_runnerOne == position_runnerTwo && position_runnerOne != 1)
+	{
+		cout << "GOTCHA " << setw(position_runnerOne) << right << setfill(' ') << 1 << setw(60 - abs(position_runnerOne)) << ' ' << endl;
+		cout << "GOTCHA " << setw(position_runnerTwo) << right << setfill(' ') << 2 << setw(60 - abs(position_runnerTwo)) << ' ' << endl;
+	}
+	else
+	{
+		cout << setw(position_runnerOne) << right << setfill('-') << 1 << setw(60 - abs(position_runnerOne)) << '-' << endl;
+		cout << setw(position_runnerTwo) << right << setfill('-') << 2 << setw(60 - abs(position_runnerTwo)) << '-' << endl;
+	}
+}
+
 int main() {
-	std::cout << "My Template" << std::endl;
+	int position_runnerOne = 1;
+	int position_runnerTwo = 1;
+	int randomNumber;
+	srand((unsigned)time(NULL));
+
+	while(true)
+	{
+		randomNumber = randomIntegerGenerator();
+		cout << "No: " << randomNumber << endl;
+		position_runnerOne = calculateRunnerOnePosition(position_runnerOne, randomNumber);
+		showRunnersLocation(position_runnerOne, position_runnerTwo);
+		if (position_runnerOne >= 60)
+		{
+			cout << "RUNNER #1 WINS!" << endl;
+			break;
+		}
+
+		randomNumber = randomIntegerGenerator();
+		cout << "No: " << randomNumber << endl;
+		position_runnerTwo = calculateRunnerTwoPosition(position_runnerTwo, randomNumber);
+		showRunnersLocation(position_runnerOne, position_runnerTwo);
+		if (position_runnerTwo >= 60)
+		{
+			cout << "RUNNER #2 WINS!" << endl;
+			break;
+		}
+	}
 
 	return 0;
 }
